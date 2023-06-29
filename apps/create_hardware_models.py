90eeab72ecbeb2c8eae4dc2d7cab48dfc7f3f3b5
John Halazonetis <john.halazonetis@icloud.com>
2023-06-29 11:12:37 +0200

    New app
    Will populate the hardware_models table in the database

--- apps/create_hardware_models.py
+++ apps/create_hardware_models.py
@@ -3,7 +3,8 @@ from typing import List
 from random import randint, choice
 
 from app_db.interface import AUProductionDB
-from app_db.sql_models import Hardware, StockStatus, BuildStatus
+from app_db.hardware_model import ModelCreator, MirrorType, MGCSize, VCMSize
+from app_db.sql_models import HardwareModel
 
 def setup_logger():
     '''Setup logger for app'''
@@ -13,51 +14,70 @@ def setup_logger():
         format='[%(asctime)s] %(levelname)-9s %(message)s'
     )
 
-def create_au() -> List[Hardware]:
+def create_model(mirror: MirrorType, position: int, version: int) -> List[HardwareModel]:
     '''
-    Create a single AU instance with partially complete parts
+    Create a single AU model instance
+
+    Parameters
+    ----------
+    - `mirror` (`MirrorType`): Mirror type
+    - `version` (`int`): Version of AU
+    - `position` (`int`): Position of AU on assigned mirror
+
+    Returns
+    -------
+    - `List[HardwareModel]`: List of hardware models to put into database
     '''
-    hardware_list = []
-    new_au = Hardware(
-        order_number = randint(200000, 999999),
-        serial_number = f'11-{randint(1000, 9999)}-{randint(1000, 9999)}',
-        material_number = f'AU-M{randint(1, 9)}',
-        parent_id = None,
-        stock_status = 'Parts Available',
-        build_status = 'Not Started',
-        position = str(choice([10, 20, 30, 13, 23, 12])),
-        version = "2"
-    )
+    creator = ModelCreator(mirror, version)
+    match mirror:
+        case MirrorType.M1:
+            mgc_size = MGCSize.Medium
+            if position == 2:
+                mgc_size = MGCSize.Small
 
-    new_vcm_1 = Hardware(
-        order_number = randint(200000, new_au.order_number),
-        serial_number = f'11-{randint(1000, 9999)}-{randint(1000, 9999)}',
-        material_number = f"VCM-{choice(['N', 'L'])}",
-        parent_id = new_au.id,
-        stock_status = StockStatus.parts_available.name,
-        build_status = BuildStatus.built.name,
-        position = '1',
-        version = '3.1'
-    )
+            return creator.create_au(str(position), mgc_size, VCMSize.Normal)
 
-    new_vcm_2 = Hardware(
-        order_number = randint(200000, new_au.order_number),
-        serial_number = f'11-{randint(1000, 9999)}-{randint(1000, 9999)}',
-        material_number = new_vcm_1.material_number,
-        parent_id = new_au.id,
-        stock_status = StockStatus.parts_available.name,
-        build_status = BuildStatus.building.name,
-        position = '2',
-        version = '3.1'
-    )
+        case MirrorType.M2:
+            mgc_size = MGCSize.Medium
+            if position == 1:
+                mgc_size = MGCSize.Small
 
-    new_mgc = Hardware(
-        order_number = randint(200000, new_au.order_number),
-        serial_number = f'11-{randint(1000, 9999)}-{randint(1000, 9999)}',
-        material_number = f"MGC-{choice(['N', 'L'])}",
-    )
+            return creator.create_au(str(position), mgc_size, VCMSize.Normal)
 
-def main(number_of_aus_to_create: int = 5):
+        case MirrorType.M3:
+            return creator.create_au(str(position), MGCSize.Medium, VCMSize.Normal)
+
+        case MirrorType.M4:
+            if position in [1, 2, 3]:
+                return creator.create_au(str(position), vcm_size=VCMSize.Normal, without_mgc=True)
+
+            else:
+                return creator.create_au(str(position), mgc_size=MGCSize.Large, without_vcm=True)
+
+        case MirrorType.M5:
+            return creator.create_au(str(position), MGCSize.Medium, VCMSize.Normal)
+
+        case MirrorType.M6:
+            mgc_size = MGCSize.Medium
+            if position == 1:
+                mgc_size = MGCSize.Small
+
+            return creator.create_au(str(position), mgc_size, vcm_size=VCMSize.Normal)
+
+        case MirrorType.M7:
+            return creator.create_au(str(position), MGCSize.Small, VCMSize.Normal)
+
+        case MirrorType.M8:
+            if position in [1, 2, 3]:
+                return creator.create_au(str(position), vcm_size=VCMSize.Large, without_mgc=True)
+
+            else:
+                return creator.create_au(str(position), mgc_size=MGCSize.Large, without_vcm=True)
+
+        case MirrorType.RSMF:
+            return creator.create_au(str(position), MGCSize.Medium, VCMSize.Normal)
+
+def main():
     pass
 
 if __name__ == '__main__':
